92eca6c28f8081a27e28fcee3ccba0f570aabcdf
Elena Pastorelli <pastorelli.elena@gmail.com>
2025-07-11 12:19:07 +0200

    Added Copyright formula compatible with GNU General Public License v3.0 - Corrected comment charachtergit add config_sim_test_Ax_and_Dendr_Delay_STDP.yaml main-sim-test-Ax-and-Dendr-Delay-STDP.py predict_stdp_alpha_forced_pl_lib.py sim_stdp_alpha_forced_pl_lib.py get_script_dir.py

--- get_script_dir.py
+++ get_script_dir.py
@@ -1,24 +1,24 @@
-/*  get_script_dir.py
- *  Copyright © 2025   Pier Stanislao Paolucci   <pier.paolucci@roma1.infn.it>
- *  Copyright © 2025   Elena Pastorelli          <elena.pastorelli@roma1.infn.it>
- *
- *  SPDX-License-Identifier: GPL-3.0-only
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+#  get_script_dir.py
+#  Copyright © 2025   Pier Stanislao Paolucci   <pier.paolucci@roma1.infn.it>
+#  Copyright © 2025   Elena Pastorelli          <elena.pastorelli@roma1.infn.it>
+#
+#  SPDX-License-Identifier: GPL-3.0-only
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
- import os
+import os
 
 def get_script_dir():
     try:
--- predict_stdp_alpha_forced_pl_lib.py
+++ predict_stdp_alpha_forced_pl_lib.py
@@ -1,22 +1,22 @@
-/*  predict_stdp_alpha_forced_pl_lib.py
- *  Copyright © 2025   Pier Stanislao Paolucci   <pier.paolucci@roma1.infn.it>
- *  Copyright © 2025   Elena Pastorelli          <elena.pastorelli@roma1.infn.it>
- *
- *  SPDX-License-Identifier: GPL-3.0-only
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+#  predict_stdp_alpha_forced_pl_lib.py
+#  Copyright © 2025   Pier Stanislao Paolucci   <pier.paolucci@roma1.infn.it>
+#  Copyright © 2025   Elena Pastorelli          <elena.pastorelli@roma1.infn.it>
+#
+#  SPDX-License-Identifier: GPL-3.0-only
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 import yaml
 import csv
